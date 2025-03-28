adb88db472c0593cfd0dc9f52b042ef746c35a25
Brady Campbell <campbellbrady09@gmail.com>
2025-03-28 14:22:56 -0400

    Update MorseCodeRestructured.ino

--- MorseCodeRestructured.ino
+++ MorseCodeRestructured.ino
@@ -1,22 +1,23 @@
 /****************************************************************
 Design by Brady Campbell
-created: 1/6/2025     last edited: 2/19/2025
+created: 1/6/2025     last edited: 3/28/2025
 Arduino Nano - Morse Code Project 
 
-This version: V5 - 1st restructure 
+This version: V1
 ****************************************************************/
-
 
 /****************************************************************
 KEY MORSE CODE FACTS
 ****************************************************************/
-//    dot 1 unit                    '.'
+//review these before making any changes 
+
+//    dot 1 unit                    '.'  
 //    dash 3 unit                   '-'
 //    intra-character space 1 unit  'na' //built into dot & dash
 //    inter-character space 3 unit  ' '
 //    word space 7 unit             '|'
-//    UNIT: 200ms 
-
+//    UNIT: 200ms  <-- IMPORTANT: this value MUST be IDENTICAL
+//                                for both units or it won't work!
 
 /****************************************************************
 DICTIONARY - ARRAY OF A STRUCT
@@ -73,6 +74,7 @@ DictionaryMorse dictionary[] = {
 //DICTIONARY SIZE
 int dictionarySize = sizeof(dictionary) / sizeof(dictionary[0]);
 
+
 /****************************************************************
 VARIABLES & PINS
 ****************************************************************/
@@ -111,16 +113,32 @@ VARIABLES & PINS
     String messageMorseOut;
     unsigned int messageMorseOutLength;
 
+    //SCRIBE & DECODING
+    float duration; 
+    bool scribeMode;
+    String messageMorseIn;
+    bool currentState; 
+    bool lastState;
+    float currentEdge;
+    float lastEdge; 
+
+
 /****************************************************************
-ENCODE ALPHANUMBERIC TO MORSE
+ENCODE ALPHANUMBERIC TO MORSE & MORSE TO ALPHA.
 ****************************************************************/
 String translateAlphaToMorse(char alpha) {
   for (int i = 0; i < dictionarySize; i++) {
     if (dictionary[i].alpha == alpha) {
-
-      //Serial.println(dictionary[i].morse); 
       return dictionary[i].morse;
+    }
+  }
+  return "!";  // Return Error Character
+}
 
+char translateMorseCharToAlphaChar(String morseSubString) {
+  for (int i = 0; i < dictionarySize; i++) {
+    if (dictionary[i].morse == morseSubString) {
+      return dictionary[i].alpha;
     }
   }
   return "!";  // Return Error Character
@@ -129,39 +147,40 @@ String translateAlphaToMorse(char alpha) {
 /****************************************************************
 TRANSLATE MORSE - PARSE AND CALL DECODE FUNCTION FOR EACH CHAR
 ****************************************************************/
-    String translateMorseToAlpha(String morseMessage){
-        String translatedMessage = ""; 
-        String morseSubString = "";
+String translateMorseToAlpha(String morseMessage) {
+    String translatedMessage = "";
+    String morseSubString = "";
 
-        for(int i=0; i< morseMessage.length(); i++){
-            char currentMorseChar = morseMessage.charAt(i);
+    for (int i = 0; i < morseMessage.length(); i++) {
+        char currentMorseChar = morseMessage.charAt(i);
 
-            if(currentMorseChar != ' '){
-                morseSubString += currentMorseChar; 
+        if (currentMorseChar != ' ' && currentMorseChar != '|') {
+            morseSubString += currentMorseChar;  // Collecting Morse character
+        } 
+        else //if (currentMorseChar == ' ' || currentMorseChar == '|') 
+        {
+            // Process the collected Morse character when a space or word separator is encountered
+            if (morseSubString.length() > 0) {
+                char translatedChar = translateMorseCharToAlphaChar(morseSubString + ' ');
+                translatedMessage += translatedChar;
+                morseSubString = ""; // Reset for next character
             }
-            else{
-                translatedMessage += translateMorseToAlpha(morseSubString);
-                morseSubString = ""; //Reset for next Char 
+            if (currentMorseChar == '|') {
+                translatedMessage += ' '; // Adding space for word separation
             }
         }
-        if (morseSubString.length() > 0) {
-            translatedMessage += translateMorseToAlpha(morseSubString);
-        }
-
-        return translatedMessage; 
-
     }
-/****************************************************************
-DECODE MORSE TO ALPHANUMBERIC
-****************************************************************/
-    char decodeMorseToAlpha(String morseSubString) {
-        for (int i = 0; i < dictionarySize; i++) {
-            if (dictionary[i].morse == morseSubString) {
-                return dictionary[i].alpha;
-            }
-        }
-    return '?';  // Return Error Character 
+    
+    // Ensure the last Morse character is translated
+    if (morseSubString.length() > 0) {
+        char translatedChar = translateMorseCharToAlphaChar(morseSubString + ' ');
+        translatedMessage += translatedChar;
     }
+
+    messageMorseIn = "";
+    return translatedMessage; 
+    
+}
 
 /****************************************************************
 MORSE SIGNAL GENERATOR 
@@ -209,56 +228,39 @@ void morseSignalGenerator() {
 /****************************************************************
 MORSE SIGNAL SCRIBE 
 ****************************************************************/
-//WIP WIP WIP WIP WIP WIP 
-void morseSignalScribe(){
+void morseSignalScribe() {
 
-	if(scribeMode == HIGH){
-		if(duration >= (0.5 * unit) && duration <= (2 * unit)){
-			messageMorseIn += '.';
-      Serial.print(".");
-
-		}
-	
-		else if(duration > (2 * unit) && duration <= (4 * unit)){
-			messageMorseIn += '-';
-      Serial.print("-");
-		}
-	
-		else{
-      //add nothing to the string
-      Serial.print("?");
-			//alert user that dot/dash too short/long
-		}
-	}
-	  
-	if(scribeMode == LOW){
-		if(duration >= (2 * unit) && duration <= ( 6 * unit)){
-			messageMorseIn += ' ';
-      Serial.print(" ");
-		}
-	
-		else if(duration > (6 * unit) && duration <= (10 * unit)){
-			messageMorseIn += '|';
-		}
-		else if(duration > (10 * unit)){
-      //add nothing, looks like end of message 
-      //handle end of message somehow
+    if (scribeMode == HIGH) {
+        if (duration >= (0.25 * unit) && duration <= (2 * unit)) {
+            messageMorseIn += '.';
+            Serial.print(".");
+        } 
+        else if (duration > (2 * unit)) {
+            messageMorseIn += '-';
+            Serial.print("-");
+        }
+        else {
+            // Add an error handler or alert for invalid signal length.
+        }
     }
-		else{
-			//do nothing, gap too short
-		}
 
-
-	}
+    if (scribeMode == LOW) {
+        if (duration >= (2 * unit) && duration <= (6 * unit)) {
+            messageMorseIn += ' ';
+            Serial.print(" "); // Intra-character gap
+        } 
+        else if (duration > (6 * unit)) {
+            messageMorseIn += '|'; // Inter-word gap
+            Serial.print("|");
+        }
+    }
 }
-	  
-  //END SCRIBE
-
+  
 /****************************************************************
 SERIAL INPUT HANDLING
 ****************************************************************/
-void serialHandler(){
-    while (Serial.available() > 0){
+void serialHandler() {
+    while (Serial.available() > 0) {
         serialIn += (char)Serial.read();
         delay(10); 
     }
@@ -268,37 +270,28 @@ void serialHandler(){
 /****************************************************************
 MODE MANAGER
 ****************************************************************/
-void modeManager(){
+void modeManager() {
     bool modeSwitch = digitalRead(ModeSwitchInputPIN);
-    //delay(10); 
-    if(modeSwitch == HIGH){
+    if (modeSwitch == HIGH) {
         //Set Mode to sending (and indicate)
         sendingMode = HIGH; 
         digitalWrite(SendingModeIndicatorPIN, HIGH); 
         receivingMode = LOW;
         digitalWrite(ReceivingModeIndicatorPIN, LOW); 
-        //Serial.println("S");
-
     }
-    else if(modeSwitch == LOW){
+    else if (modeSwitch == LOW) {
         //Set Mode to Receiving (and indicate)
         sendingMode = LOW; 
         digitalWrite(SendingModeIndicatorPIN, LOW); 
         receivingMode = HIGH;
         digitalWrite(ReceivingModeIndicatorPIN, HIGH); 
-        //Serial.println("R");
- 
     }
 }
 
 /****************************************************************
 SETUP - RUNS JUST ONCE, LIGHT USE IN THIS PROGRAM
 ****************************************************************/
-//SET BAUD
-//SET PIN MODES 
-//GIVE USER INSTRUCTIONS
-void setup(){
-
+void setup() {
     //SETUP 
     Serial.begin(9600);
     
@@ -312,39 +305,35 @@ void setup(){
 
     //USER INSTRUCTIONS 
     Serial.println("OPEN SERIAL, AWAITING USER INPUT");
-
-
 }
 
 /****************************************************************
 LOOP - RUNS REPEATEDLY, MAIN BLOCK OF CODE FOR THIS PROGRAM 
 ****************************************************************/
-void loop(){
+void loop() {
 
     modeManager();
+
     /************************************************************
     SENDING 
     ************************************************************/
-    if(receivingMode == HIGH){ 
-
-        //SERIAL INPUT HANDLING
+    if (sendingMode == HIGH) { 
         serialHandler(); 
 
         //TRANSLATE ALPHANUMBERIC TO MORSE
-        for(int i = 0; i < serialInLength; i++){
+        for (int i = 0; i < serialInLength; i++) {
             messageMorseOut += translateAlphaToMorse(serialIn[i]);
         }
 
-      
+        if (serialIn != "") {
+            //PRINT TO SERIAL
+            Serial.println("SENDING MODE, YOUR MESSAGE: ");
+            Serial.println("ALPHANUMBERIC:");
+            Serial.println(serialIn);
+            Serial.println("MORSE-ENCODED: "); 
+            Serial.println(messageMorseOut); 
+        }
 
-      if(serialIn != ""){
-        //PRINT TO SERIAL
-        Serial.println("SENDING MODE, YOUR MESSAGE: ");
-        Serial.println("ALPHANUMBERIC:");
-        Serial.println(serialIn);
-        Serial.println("MORSE-ENCODED: "); 
-        Serial.println(messageMorseOut); 
-      }
         //SIGNAL GENERATOR
         messageMorseOutLength = messageMorseOut.length(); 
         morseSignalGenerator(); 
@@ -355,38 +344,43 @@ void loop(){
     /************************************************************
     RECEIVING 
     ************************************************************/
-    else if(sendingMode == HIGH){ //CHECK IF RECEIVING 
+    else if (receivingMode == HIGH) { 
 
-        //DIGITAL INPUT HANDLING
-
-
-        currentState = digitalRead(MorseInPin);
-	
-        if (lastState == LOW && currentState == HIGH){
-          scribeMode = LOW; 
-          currentEdge = millis();
-          duration = currentEdge - lastEdge; 
-          morseSignalScribe(); //pass duration and mode to scribe 
-          lastEdge = millis();
+        currentState = digitalRead(SignalInPIN);
+    
+        if (lastState == LOW && currentState == HIGH) {
+            scribeMode = LOW; 
+            currentEdge = millis();
+            duration = currentEdge - lastEdge; 
+            morseSignalScribe(); //pass duration and mode to scribe 
+            lastEdge = millis();
         }
 
-        if (lastState == HIGH && currentState == LOW){
-          scribeMode = HIGH; 
-          currentEdge = millis();
-          duration = currentEdge - lastEdge;
-          morseSignalScribe(); //pass duration and mode to scribe
-          lastEdge = millis();
+        if (lastState == HIGH && currentState == LOW) {
+            scribeMode = HIGH; 
+            currentEdge = millis();
+            duration = currentEdge - lastEdge;
+            morseSignalScribe(); //pass duration and mode to scribe
+            lastEdge = millis();
         }
 
-        If(duration >= 10 * units){
-          
+        if (lastState == LOW && lastState == currentState) {
+            currentEdge = millis();
+            duration = currentEdge - lastEdge;
+
+            if(duration >= 20 * unit){
+                //TRANSLATE MORSE TO ALPHANUMBERIC
+                if(messageMorseIn != ""){
+                    Serial.println();
+                    Serial.println("MORSE MESSAGE RECEIVED");
+                    Serial.println(messageMorseIn);
+                    Serial.println("TRANSLATED MESSAGE");
+                    Serial.println(translateMorseToAlpha(messageMorseIn));
+
+                }
+            }
         }
-  
-         lastState = currentState; //update state
 
-        //TRANSLATE MORSE TO ALPHANUMBERIC
-
-        //OUTPUT TO SERIAL
-        
+        lastState = currentState; //update state
     }
 }
